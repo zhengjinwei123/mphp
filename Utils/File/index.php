2f7d73e96f1deee16e7706943afe97eeafb19b73
zhengjinwei123 <2538698032@qq.com>
2017-03-30 16:06:49 +0800

    update

--- Utils/File/index.php
+++ Utils/File/index.php
@@ -93,5 +93,84 @@ class FileUtil
         }
         unlink($filePath);
     }
+
+    // PHP数组转换成字串
+    public static function phpArrayEval($array, $level = 0)
+    {
+        $space = '';
+        for($i = 0; $i <= $level; $i++) {
+            $space .= "\t";
+        }
+
+        $evaluate = "Array\n{$space}(\n";
+
+        $comma = $space;
+        foreach($array as $key => $val) {
+            $key = is_string($key) ? '\''.addcslashes($key, '\'\\').'\'' : $key;
+            $val = !is_array($val) && (!preg_match("/^\-?\d+$/", $val) || strlen($val) > 12 || substr($val, 0, 1)=='0') ? '\''.addcslashes($val, '\'\\').'\'' : $val;
+
+            if(is_array($val)) {
+                $evaluate .= "{$comma}{$key} => " . self::phpArrayeval($val, $level + 1);
+            } else {
+                $evaluate .= "{$comma}{$key} => {$val}";
+            }
+
+            $comma = ",\n$space";
+        }
+
+        $evaluate .= "\n$space)";
+
+        return $evaluate;
+    }
+
+    //去除代码中的空白和注释
+    public static function stripWhitespace($content)
+    {
+        $stripStr   = '';
+
+        //分析php源码
+        $tokens     = token_get_all($content);
+        $last_space = false;
+
+        for ($i = 0, $j = count($tokens); $i < $j; $i++) {
+            if (is_string($tokens[$i])) {
+                $last_space = false;
+                $stripStr  .= $tokens[$i];
+            } else {
+                switch ($tokens[$i][0]) {
+                    //过滤各种PHP注释
+                    case T_COMMENT:
+                    case T_DOC_COMMENT:
+                        break;
+                    //过滤空格
+                    case T_WHITESPACE:
+                        if (!$last_space) {
+                            $stripStr  .= ' ';
+                            $last_space = true;
+                        }
+                        break;
+                    case T_START_HEREDOC:
+                        $stripStr .= "<<<ZBDOC\n";
+                        break;
+                    case T_END_HEREDOC:
+                        $stripStr .= "ZBDOC;\n";
+                        for($k = $i+1; $k < $j; $k++) {
+                            if(is_string($tokens[$k]) && $tokens[$k] == ';') {
+                                $i = $k;
+                                break;
+                            } else if($tokens[$k][0] == T_CLOSE_TAG) {
+                                break;
+                            }
+                        }
+                        break;
+                    default:
+                        $last_space = false;
+                        $stripStr  .= $tokens[$i][1];
+                }
+            }
+        }
+
+        return $stripStr;
+    }
 }
 
